25d6ec2f9bc95e52d77eb4c8ad633dea7df3d0d7
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-09-01 06:49:29 +0300

    apply our folder structure and out main tab view

---
+++ TinderTutorial/App/TinderTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialApp.swift
+//  TinderTutorial
+//
+//  Created by Ahmed Fouad on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TinderTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TinderTutorialTests/TinderTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  TinderTutorialTests.swift
+//  TinderTutorialTests
+//
+//  Created by Ahmed Fouad on 01/09/2025.
+//
+
+import Testing
+@testable import TinderTutorial
+
+struct TinderTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
